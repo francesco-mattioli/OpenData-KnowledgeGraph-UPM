dccdf0e1154aceaa8de04fb2d346b2e27213c68b
Teemo <34406602+Tymotheus@users.noreply.github.com>
2023-11-07 13:52:20 +0100

    Updated the app to use knowledge graph. Added some endpoints, return jsons now. station-by-city not loading in docs but working

--- HandsOn/Group07/app/app/main.py
+++ HandsOn/Group07/app/app/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI, APIRouter, Query, HTTPException, Request
 from fastapi.templating import Jinja2Templates
 
 from pathlib import Path
-import rdflib
+from rdflib import Graph
 from rdflib_endpoint import SparqlRouter
+from rdflib.plugins.sparql import prepareQuery
 from typing import Optional, Any
 
 from app.schemas import ChargingStation, StationSearchResults
@@ -29,62 +30,103 @@ def root(request: Request) -> dict:
     )
 
 
-@api_router.get("/station/{station_id}", status_code=200, response_model=ChargingStation)
-def fetch_station(*, station_id: int) -> Any:
-    """
-    Fetch a single station by ID
-    :param station_id: number of the recipe
-    :return:
-    """
-
-    result = [station for station in STATIONS if station["id"] == station_id]
-    if not result:
-        raise HTTPException(
-            status_code=404, detail=f"Station with ID {station_id} not found"
-        )
-    return result[0]
-
-
 @api_router.get("/search/", status_code=200, response_model=StationSearchResults)
 def search_station(
         keyword: Optional[str] = Query(None, min_length=3, examples="BMW"),
         max_results: Optional[int] = 10
 ) -> dict:
     """
-    Search for stations based on label keyword
-    :param keyword:
-    :param max_results:
-    :return:
+    To implement: Search for stations based on label keyword
     """
-    if not keyword:
-        return {"results": STATIONS[:max_results]}
-    results = filter(lambda station: keyword.lower() in station["charging_station"].lower(), STATIONS)
-    return {"results": list(results)[:max_results]}
+    pass
 
 
-@api_router.get("/search-city/", status_code=200, response_model=StationSearchResults)
-def search_city(
-        keyword: Optional[str] = None, max_results: Optional[int] = 10
+@api_router.get("/cities/", status_code=200)
+def get_cities() -> any:
+    """Get all the cities existing in the knowledge graph."""
+    g = Graph().parse(str(BASE_PATH / "data" / "rdf-withlinks.ttl"), format="turtle")
+    query = prepareQuery("""
+    PREFIX ont: <https://www.chargeup.io/group07/ontology#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    SELECT ?city ?label ?dbpedia_city WHERE {
+        ?city a ont:City .
+        ?city rdfs:label ?label .
+        ?city <http://www.w3.org/2002/07/owl#sameAs> ?dbpedia_city
+    }
+    """)
+    results = g.query(query)
+    output = [{"city": str(result[0]),
+               "label": str(result[1]),
+               "dbpedia_city": str(result[2])
+               } for result in results]
+    return output
+
+
+@api_router.get("/stations/", status_code=200)
+def get_stations() -> any:
+    """Get all the stations and their street adresses"""
+    g = Graph().parse(str(BASE_PATH / "data" / "rdf-withlinks.ttl"), format="turtle")
+    query = prepareQuery("""
+        PREFIX ont: <https://www.chargeup.io/group07/ontology#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT ?station ?label ?address WHERE {
+            ?station a ont:ChargingStation .
+            ?station ont:hasStreetAddress ?address .
+            ?station rdfs:label ?label .
+        }
+        """)
+    results = g.query(query)
+    output = [
+              {"station": str(result[0]),
+               "label": str(result[1]),
+               "address": str(result[2]),
+               }
+              for result in results]
+    return output
+
+
+@api_router.get("/station-by-city/", status_code=200)
+def get_station_by_city(
+        city: Optional[str] = Query(None, min_length=3, examples="Darien"),
+        max_results: Optional[int] = 10
 ) -> dict:
-    """
-    Search for stations based on label keyword
-    :param keyword:
-    :param max_results:
-    :return:
-    """
-    if not keyword:
-        return {"results": STATIONS[:max_results]}
-    results = filter(lambda station: keyword.lower() in station["city"].lower(), STATIONS)
-    return {"results": list(results)[:max_results]}
+    """To implement: lower case indifference"""
+    g = Graph().parse(str(BASE_PATH / "data" / "rdf-withlinks.ttl"), format="turtle")
+    query = prepareQuery(f"""
+    PREFIX ont: <https://www.chargeup.io/group07/ontology#>
+    SELECT ?station ?address ?city
+    WHERE {{
+        ?station a ont:ChargingStation .
+        ?station ont:hasStreetAddress ?address .
+        ?address ont:hasCity ?city .
+        ?city rdfs:label "{city}" .
+    }}
+    LIMIT {max_results}
+    """)
+    results = g.query(query)
+    output = [
+        {"station": str(result[0]),
+         "address": str(result[1]),
+         "city": str(result[2]),
+         }
+        for result in results]
+    return output
 
 
 sparql_router = SparqlRouter(
-    graph=rdflib.Graph().parse(str(BASE_PATH / "data" / "rdf.ttl"), format="turtle"),
+    graph=Graph().parse(str(BASE_PATH / "data" / "rdf-withlinks.ttl"), format="turtle"),
     path="/chargy/",
     title="SPARQL endpoint for RDFLib graph",
     description='Our charger sparql endpoint',
     version="0.1.0",
     public_url='https://my-endpoint-url/',
+    example_query="""
+    PREFIX ont: <https://www.chargeup.io/group07/ontology#>
+    SELECT ?city WHERE {
+        ?city a ont:City .
+    }
+    LIMIT 5
+    """
 )
 app.include_router(api_router)  # Basic router coming from FastAPI
 app.include_router(sparql_router)  # Router taken from rdflib
